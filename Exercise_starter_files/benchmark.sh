e3f582fd301dfe9228506db167a5f626febc95db
Sayar Banerjee <30318719+Sayar1106@users.noreply.github.com>
2021-01-01 14:32:40 +0000

    Add files via upload

--- Exercise_starter_files/benchmark.sh
+++ Exercise_starter_files/benchmark.sh
@@ -70,4 +70,4 @@
 # Total:        114  160  28.0    172     199#
 
 
- ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer REPLACE_WITH_KEY' http://REPLACE_WITH_API_URL/score
+ ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer 83JgIWVpDYNFyCbauImEclquHjMQSVsU' http://6cec4af7-1264-49c9-9388-bb5b9a5d97ef.southcentralus.azurecontainer.io/score
--- Exercise_starter_files/logs.py
+++ Exercise_starter_files/logs.py
@@ -5,11 +5,14 @@ from azureml.core.webservice import Webservice
 ws = Workspace.from_config()
 
 # Set with the deployment name
-name = ""
+name = "bank-marketing-model"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
-logs = service.get_logs()
 
+# Enabling app insights
+service.update(enable_app_insights=True)
+
+logs = service.get_logs()
 for line in logs.split('\n'):
     print(line)
